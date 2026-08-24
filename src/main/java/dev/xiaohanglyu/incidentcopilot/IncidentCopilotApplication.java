f27168a0f9a9c11a0c0b18e5ab5ce75e3b4fce3a
lynlyudev <lynlyu.dev@gmail.com>
2026-08-24 13:45:14 -0700

    chores: init

---
+++ src/main/java/dev/xiaohanglyu/incidentcopilot/IncidentCopilotApplication.java
@@ -0,0 +1,12 @@
+package dev.xiaohanglyu.incidentcopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentCopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentCopilotApplication.class, args);
+    }
+}
